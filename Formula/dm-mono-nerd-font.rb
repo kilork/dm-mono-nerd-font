fcf4157e0633e5d7d9c9543dfa29d5278f70b774
Alexander Korolev <alexander.korolev.germany@gmail.com>
2026-03-21 23:37:34 +0100

    Fix YAML syntax for tap update

--- Formula/dm-mono-nerd-font.rb
+++ Formula/dm-mono-nerd-font.rb
@@ -1,21 +1,18 @@
 class DmMonoNerdFont < Formula
   desc "DM Mono with Nerd Fonts glyphs"
   homepage "https://github.com/kilork/dm-mono-nerd-font"
-  version "3.4.0"
+  version "{{VERSION}}"
 
-  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v3.4.0/dm-mono-nerd-font-3.4.0.tar.gz"
-  sha256 "84757679bef5b379364691a7d9995d03e9afa40c5cacc39e3cb428e4db1479a1"
+  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v{{VERSION}}/dm-mono-nerd-font-{{VERSION}}.tar.gz"
+  sha256 "{{SHA256}}"
 
   def install
-    system "tar", "-xzf", cached_download, "--strip-components=1", "-C", prefix
-  end
+    font_dir = Pathname.new(ENV["HOME"])/"Library/Fonts"
+    font_dir.mkpath
 
-  def caveats
-    <<~EOS
-      Fonts are installed to #{prefix}.
-      To make them available system-wide, copy to ~/Library/Fonts:
-        cp #{prefix}/*.ttf ~/Library/Fonts/
-    EOS
+    Dir["*.ttf"].each do |font|
+      cp font, font_dir
+    end
   end
 
   test do
