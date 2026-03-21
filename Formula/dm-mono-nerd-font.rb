22b3b7c5c588efb7411e88956c03f7bc52a63489
GitHub Action <action@github.com>
2026-03-21 22:25:53 +0000

    Update formula to v3.4.0

--- Formula/dm-mono-nerd-font.rb
+++ Formula/dm-mono-nerd-font.rb
@@ -1,18 +1,21 @@
-class DmMonoNerdFont
+class DmMonoNerdFont < Formula
   desc "DM Mono with Nerd Fonts glyphs"
   homepage "https://github.com/kilork/dm-mono-nerd-font"
-  version "1.0.0"
+  version "3.4.0"
 
-  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v1.0.0/dm-mono-nerd-font-1.0.0.tar.gz"
-  sha256 "1bd78369eb35c7a735d6269f6d6b36b41bebabdb540c0f291e8babd83fbe6f6f"
+  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v3.4.0/dm-mono-nerd-font-3.4.0.tar.gz"
+  sha256 "84757679bef5b379364691a7d9995d03e9afa40c5cacc39e3cb428e4db1479a1"
 
   def install
-    font_dir = Pathname.new(ENV["HOME"])/"Library/Fonts"
-    font_dir.mkpath
+    system "tar", "-xzf", cached_download, "--strip-components=1", "-C", prefix
+  end
 
-    Dir["*.ttf"].each do |font|
-      cp font, font_dir
-    end
+  def caveats
+    <<~EOS
+      Fonts are installed to #{prefix}.
+      To make them available system-wide, copy to ~/Library/Fonts:
+        cp #{prefix}/*.ttf ~/Library/Fonts/
+    EOS
   end
 
   test do
