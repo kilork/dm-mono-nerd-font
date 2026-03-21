2f7b0bc1f69f61691ad2f93e99368a294733f6bb
GitHub Action <action@github.com>
2026-03-21 20:51:11 +0000

    Update formula to v1.0.0

--- Formula/dm-mono-nerd-font.rb
+++ Formula/dm-mono-nerd-font.rb
@@ -1,10 +1,10 @@
 class DmMonoNerdFont
   desc "DM Mono with Nerd Fonts glyphs"
   homepage "https://github.com/kilork/dm-mono-nerd-font"
-  version "3.4.0"
+  version "1.0.0"
 
-  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v#{version}/dm-mono-nerd-font-#{version}.tar.gz"
-  sha256 "TODO"
+  url "https://github.com/kilork/dm-mono-nerd-font/releases/download/v1.0.0/dm-mono-nerd-font-1.0.0.tar.gz"
+  sha256 "1bd78369eb35c7a735d6269f6d6b36b41bebabdb540c0f291e8babd83fbe6f6f"
 
   def install
     font_dir = Pathname.new(ENV["HOME"])/"Library/Fonts"
